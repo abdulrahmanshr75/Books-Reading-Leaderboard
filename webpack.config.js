b6ed0ed3c86bc9e2b83c6b10d20cb1040946a314
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-02-02 00:15:12 +0200

    add basic setup files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
