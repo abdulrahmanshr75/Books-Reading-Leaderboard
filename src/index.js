94da4e503ab398f43f8010477a0a0732e9d936b2
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2022-02-02 23:32:56 +0200

    add functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+import { updateTheScores, getTheScores } from './modules/api.js';
+import displayScores from './modules/displayScores.js';
+
+window.onload = async () => {
+  displayScores(await getTheScores());
+};
+const refresh = document.querySelector('.btn');
+
+refresh.addEventListener('click', async () => {
+  const list = await getTheScores();
+  displayScores(list);
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const printScore = {
+    user: document.querySelectorAll('input')[0].value.trim(),
+    score: document.querySelectorAll('input')[1].value.trim(),
+  };
+  form.reset();
+  await updateTheScores(printScore);
+});
